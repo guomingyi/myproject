1fceee8bb0eba64d596d2e3494592c5e4c2c408c
mingyi.guo <mingyi.guo@tinno.com>
2018-05-24 08:53:07 +0800

    add download.sh & gdb debug.sh

---
+++ mcu/download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ACTION=$1
+
+if [ "y$ACTION" = "yb" ] ;then
+  echo -e "\033[32m\n bootloader-bl1.4.0.bin 0x08000000. \033[0m"
+  st-flash write build/bootloader-bl1.4.0.bin 0x08000000
+  exit 0
+fi
+
+if [ "y$ACTION" = "yf" ]; then
+  echo -e "\033[32m\n firmware-v1.6.1.bin 0x08010000. \033[0m"
+  st-flash write build/firmware-v1.6.1.bin 0x08014000
+  exit 0
+fi
+
+echo -e "\033[32m\n bootloader-bl1.4.0.bin 0x08000000. \033[0m"
+st-flash write build/bootloader-bl1.4.0.bin 0x08000000 && \
+
+echo -e "\033[32m\n firmware-v1.6.1.bin 0x08010000. \033[0m"
+st-flash write build/firmware-v1.6.1.bin 0x08010000
+
